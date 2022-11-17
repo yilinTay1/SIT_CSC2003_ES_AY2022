aef6879382b6dec4bba44a9265f3482ad905c200
Yi Lin ♡ <69491858+yilinTay1@users.noreply.github.com>
2022-11-18 02:22:12 +0800

    Lalalala

---
+++ projects/ESP01HC05 via UART/Pico-HC05_Receiving.c
@@ -0,0 +1,87 @@
+#include "hardware/uart.h"
+#include "hardware/irq.h"
+#include "pico/stdlib.h"
+#include <string.h>
+
+#define UART_ID uart0
+#define BAUD_RATE 9600
+#define DATA_BITS 8
+#define STOP_BITS 1
+#define PARITY UART_PARITY_NONE
+
+// We are using pins 0 and 1, but see the GPIO function select table in the
+// datasheet for information on which other pins can be used.
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+// Negative number flag
+int flagNegative = 0;
+
+// RX interrupt handler
+void on_uart_rx()
+{
+    while (uart_is_readable(UART_ID))
+    {
+        uint8_t ch = uart_getc(UART_ID);
+
+        if (uart_is_writable(UART_ID))
+        {
+            if (ch == 45)
+            { // Negative number 1st character E.g. -5 to -1, (-)
+                flagNegative = 1;
+                uart_putc(UART_ID, ch);
+            }
+            else if (flagNegative == 1)
+            { // Negative number 2nd character E.g. -5 to -1, (5)
+                uart_putc(UART_ID, ch);
+                uart_puts(UART_ID, "\nHello, Negative Number\n");
+                flagNegative = 0;
+            }
+            else
+            { // Positive number 1st character E.g. 0 - 5
+                uart_putc(UART_ID, ch);
+                uart_puts(UART_ID, "\nHello, Positive Number\n");
+            }
+        }
+    }
+}
+
+int main()
+{
+    // Set up our UART with a basic baud rate.
+    uart_init(UART_ID, 2400);
+
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+    // Actually, we want a different speed
+    // The call will return the actual baud rate selected, which will be as close as
+    // possible to that requested
+    int __unused actual = uart_set_baudrate(UART_ID, BAUD_RATE);
+
+    // Set UART flow control CTS/RTS, we don't want these, so turn them off
+    uart_set_hw_flow(UART_ID, false, false);
+
+    // Set our data format
+    uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
+
+    // Turn off FIFO's - we want to do this character by character
+    uart_set_fifo_enabled(UART_ID, false);
+
+    // Set up a RX interrupt
+    // We need to set up the handler first
+    // Select correct interrupt for the UART we are using
+    int UART_IRQ = UART_ID == uart0 ? UART0_IRQ : UART1_IRQ;
+
+    // And set up and enable the interrupt handlers
+    irq_set_exclusive_handler(UART_IRQ, on_uart_rx);
+    irq_set_enabled(UART_IRQ, true);
+
+    // Now enable the UART to send interrupts - RX only
+    uart_set_irq_enables(UART_ID, true, false);
+
+    while (1)
+        tight_loop_contents();
+}
---
+++ projects/ESP01HC05 via UART/Pico-HC05_Sending.c
@@ -0,0 +1,28 @@
+//Example script of USB serial
+
+#include "pico/stdlib.h"
+#include <stdio.h>
+#include "hardware/uart.h"
+
+#define UART_ID uart0
+#define BAUD_RATE 9600
+
+// We are using pins 0 and 1, but see the GPIO function select table in the
+// datasheet for information on which other pins can be used.
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+int main() {
+    // Set up our UART with the required speed.
+    uart_init(UART_ID, BAUD_RATE);
+
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+ 
+    while (true) { 
+        uart_puts(UART_ID, " Hello, UART!\n");
+        sleep_ms(1000);
+    }
+}
